aef4f273cb7c9e42d598ecafece354ada3c5f27f
Ismarji Workman <ismarjiw@protonmail.com>
2023-09-24 18:12:26 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Angular Tutorial 
+[Intro to Angular](https://www.youtube.com/watch?v=xAT0lHYhHMY&list=PL1w1q3fL4pmj9k1FrJ3Pe91EPub2_h4jF)
+### To start angular server: 
+bun start
+### To start json-server: 
+json-server --watch db.json
--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -52,9 +52,12 @@ export class DetailsComponent {
     lastName: new FormControl(''),
     email: new FormControl('')
   })
+
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocation(housingLocationId);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   submitApplication() {
